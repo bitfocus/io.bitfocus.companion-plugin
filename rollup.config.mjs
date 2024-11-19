d3b6d32909198149239b1bb36c8f658b917368cd
Joseph Adams <josephdadams@gmail.com>
2024-11-18 22:17:37 -0500

    prettier

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,74 +1,73 @@
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
-import image from "@rollup/plugin-image";
-import path from "node:path";
-import url from "node:url";
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import image from '@rollup/plugin-image'
+import path from 'node:path'
+import url from 'node:url'
 
-const isWatching = !!process.env.ROLLUP_WATCH;
-const sdPlugin = "io.bitfocus.companion-plugin.sdPlugin";
+const isWatching = !!process.env.ROLLUP_WATCH
+const sdPlugin = 'io.bitfocus.companion-plugin.sdPlugin'
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const pluginConfig = {
-	input: "src/plugin.ts",
+	input: 'src/plugin.ts',
 	output: {
 		file: `${sdPlugin}/bin/plugin.js`,
 		sourcemap: isWatching,
 		sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
-			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href;
-		}
+			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href
+		},
 	},
 	plugins: [
 		{
-			name: "watch-externals",
+			name: 'watch-externals',
 			buildStart: function () {
-				this.addWatchFile(`${sdPlugin}/manifest.json`);
+				this.addWatchFile(`${sdPlugin}/manifest.json`)
 			},
 		},
 		typescript({
-			mapRoot: isWatching ? "./" : undefined
+			mapRoot: isWatching ? './' : undefined,
 		}),
 		nodeResolve({
 			browser: false,
-			exportConditions: ["node"],
-			preferBuiltins: true
+			exportConditions: ['node'],
+			preferBuiltins: true,
 		}),
 		commonjs(),
 		image(),
 		!isWatching && terser(),
 		{
-			name: "emit-module-package-file",
+			name: 'emit-module-package-file',
 			generateBundle() {
-				this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
-			}
-		}
-	]
-};
-
+				this.emitFile({ fileName: 'package.json', source: `{ "type": "module" }`, type: 'asset' })
+			},
+		},
+	],
+}
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const inspectorConfig = {
-	input: "src/inspector.ts",
+	input: 'src/inspector.ts',
 	output: {
 		file: `${sdPlugin}/bin/inspector.js`,
 		sourcemap: isWatching,
 		sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
-			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href;
-		}
+			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href
+		},
 	},
 	plugins: [
 		typescript({
-			mapRoot: isWatching ? "./" : undefined
+			mapRoot: isWatching ? './' : undefined,
 		}),
 		nodeResolve({
 			browser: true,
-			exportConditions: ["node"],
-			preferBuiltins: true
+			exportConditions: ['node'],
+			preferBuiltins: true,
 		}),
 		commonjs(),
 		!isWatching && terser(),
@@ -78,7 +77,7 @@ const inspectorConfig = {
 		// 		this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
 		// 	}
 		// }
-	]
-};
+	],
+}
 
-export default [pluginConfig, inspectorConfig];
+export default [pluginConfig, inspectorConfig]
