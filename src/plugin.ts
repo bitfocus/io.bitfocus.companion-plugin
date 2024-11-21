4bca70f95300cbd4d4564685926b88e68409e9c8
Joseph Adams <josephdadams@gmail.com>
2024-11-21 13:55:15 -0500

    add support for global settings

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,6 @@
-import streamDeck, { LogLevel } from '@elgato/streamdeck'
+import { GlobalSettings } from './types/types'
+
+import streamDeck, { LogLevel, DidReceiveGlobalSettingsEvent } from '@elgato/streamdeck'
 
 import { connection } from './companion-connection'
 
@@ -14,6 +16,44 @@ streamDeck.actions.registerAction(mainAction)
 // Finally, connect to the Stream Deck.
 streamDeck.connect()
 
+// Get the global settings
+streamDeck.settings.getGlobalSettings()
+
+// Global settings received
+streamDeck.settings.onDidReceiveGlobalSettings((settings: DidReceiveGlobalSettingsEvent<GlobalSettings>) => {
+	console.log('Got global settings:', settings)
+	streamDeck.logger.info('Got global settings')
+	streamDeck.logger.info(settings)
+
+	let ip = settings.settings.ip
+	streamDeck.logger.info('ip', ip)
+	if (!ip) {
+		ip = '127.0.0.1'
+	}
+
+	let port = settings.settings.port
+	streamDeck.logger.info('port', port)
+	if (!port) {
+		port = 28492
+	}
+
+	let connectionStatus = settings.settings.connectionStatus
+	if (!connectionStatus) {
+		connectionStatus = ''
+	}
+
+	streamDeck.settings.setGlobalSettings({ ip: ip, port: port, connectionStatus: connectionStatus }).catch((e) => {
+		streamDeck.logger.warn(`Failed to save global settings: ${e}`)
+	})
+
+	streamDeck.logger.info('Connecting to Companion at', ip, port)
+
+	connection.setAddress(ip)
+	connection.setPort(port)
+
+	connection.connect()
+})
+
 connection.on('wrongversion', () => {
 	streamDeck.logger.info('wrong version')
 
@@ -21,13 +61,13 @@ connection.on('wrongversion', () => {
 })
 
 connection.on('connected', () => {
-	streamDeck.logger.info('conneced')
+	streamDeck.logger.info('Connected to Companion')
 
 	mainAction.connectionStateChange()
 
 	// console.log('New device with plugin UUID: ', pluginUUID)
 	// 	companionClient.removeAllListeners("new_device:result");
-	connection.apicommand('new_device', { id: 'temp_id', supportsPng: true, supportsCoordinates: true })
+	connection.apiCommand('new_device', { id: 'temp_id', supportsPng: true, supportsCoordinates: true })
 	connection.once('new_device:result', (res) => {
 		console.log('New device result:', res)
 
@@ -50,9 +90,7 @@ connection.on('clearAllKeys', () => {
 })
 
 connection.on('disconnect', () => {
-	streamDeck.logger.info('disconneced')
+	streamDeck.logger.info('Disconnected from Companion')
 
 	mainAction.connectionStateChange()
 })
-
-connection.connect()
